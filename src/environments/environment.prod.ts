3b5d8570a6419fcde239ba8a2d086c5e65100c30
Corentin <$Blender1>
2019-11-19 10:14:55 +0100

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+   apiKey: "AIzaSyBbXWhdiHczCg57NBY07d6IitUAXHcYuKE",
+    authDomain: "projettest-d658c.firebaseapp.com",
+	databaseURL: "https://projettest-d658c.firebaseio.com",
+    projectId: "projettest-d658c",
+    storageBucket: "projettest-d658c.appspot.com",
+    messagingSenderId: "217640882852",
+    appId: "1:217640882852:web:979079367f1244327d0c31",
+    measurementId: "G-E7CJSWGVR4"
+ }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBbXWhdiHczCg57NBY07d6IitUAXHcYuKE",
+    authDomain: "projettest-d658c.firebaseapp.com",
+    databaseURL: "https://projettest-d658c.firebaseio.com",
+    projectId: "projettest-d658c",
+    storageBucket: "projettest-d658c.appspot.com",
+    messagingSenderId: "217640882852",
+    appId: "1:217640882852:web:979079367f1244327d0c31",
+    measurementId: "G-E7CJSWGVR4"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
