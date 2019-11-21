f213b5636674dbd38940dbca58c36445cb93d05c
Mathieu <math.stiv@gmail.com>
2019-11-21 10:00:10 +0100

    Ajout bouton flottant + méthode addMarker()

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,71 +15,73 @@ import { //Import des plugins GoogleMaps nécessaires
 import {Geolocation} from '@ionic-native/geolocation';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  map: GoogleMap; //Instance de carte
-loading: any;
-
-  constructor(
-      public alertController: AlertController,
-      public actionCtrl: ActionSheetController,
-      public loadingCtrl: LoadingController,
-      public toastCtrl: ToastController,
-      private platform: Platform
-  ) {
-    if (this.platform.is('cordova')) {
-      this.loadMap();
-      this.getPosition();
+    map: GoogleMap; //Instance de carte
+    loading: any
+    location: any;
+    labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    labelIndex = 0;
+    constructor(
+        public alertController: AlertController,
+        public actionCtrl: ActionSheetController,
+        public loadingCtrl: LoadingController,
+        public toastCtrl: ToastController,
+        private platform: Platform
+    ) {
+        if (this.platform.is('cordova')) {
+            this.loadMap();
+            this.getPosition();
+        }
     }
-  }
 
-  loadMap() {
-    Environment.setEnv({
-      API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyB_6JvTaVpooi1IF96UN4Cw-lo1nM9yfUo',
-      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyB_6JvTaVpooi1IF96UN4Cw-lo1nM9yfUo'
+    loadMap() {
+        Environment.setEnv({
+            API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyB_6JvTaVpooi1IF96UN4Cw-lo1nM9yfUo',
+            API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyB_6JvTaVpooi1IF96UN4Cw-lo1nM9yfUo'
+        });
+
+        this.map = GoogleMaps.create('map_canvas', {});
+    }
+
+    async getPosition() {
+        this.map.setOptions({zoomControl : false});
+        this.loading = await this.loadingCtrl.create({
+            message: 'Patientez...'
+        });
+        await this.loading.present();
+
+        // Récupération de la géolocalisation
+        this.map.getMyLocation().then((location: MyLocation) => {
+            this.loading.dismiss();
+            this.location = location;
+            console.log(JSON.stringify(location, null, 2));
+
+            // Animation de caméra
+            this.map.animateCamera({
+                target: location.latLng,
+                zoom: 18,
+                tilt: 30
+            });
+
+            // Ajout d'un marker
+            let marker: Marker = this.map.addMarkerSync({
+                title: 'Position',
+                snippet: 'Vous êtes ici !',
+                position: location.latLng,
+                animation: GoogleMapsAnimation.BOUNCE
+            });
+        });
+    }
+
+    addMarker(location){
+        let marker: Marker = this.map.addMarkerSync({
+            position: location,
+            label: this.labels[this.labelIndex++ % this.labels.length],
+            animation: GoogleMapsAnimation.BOUNCE
     });
-
-    this.map = GoogleMaps.create('map_canvas', {
-      camera: {
-        target: {
-          lat: 12, //Ici, set geolocalisation actuelle
-          lng: 3.876716
-        },
-        zoom: 12, //Augmenter le zoom
-        tilt: 30
-      },
-      zoomControl: false
-    });
-  }
-
-  async getPosition() {
-      this.loading = await this.loadingCtrl.create({
-          message: 'Please wait...'
-      });
-      await this.loading.present();
-
-      // Get the location of you
-      this.map.getMyLocation().then((location: MyLocation) => {
-          this.loading.dismiss();
-          console.log(JSON.stringify(location, null, 2));
-
-          // Move the map camera to the location with animation
-          this.map.animateCamera({
-              target: location.latLng,
-              zoom: 17,
-              tilt: 30
-          });
-
-          // add a marker
-          let marker: Marker = this.map.addMarkerSync({
-              title: '@ionic-native/google-maps plugin!',
-              snippet: 'This plugin is awesome!',
-              position: location.latLng,
-              animation: GoogleMapsAnimation.BOUNCE
-          });
-  });
-  }
+    }
 }
