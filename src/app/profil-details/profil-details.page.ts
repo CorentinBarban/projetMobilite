7c734ffdf20165a86b42db21b5c69acef9b81447
Mathieu <math.stiv@gmail.com>
2019-11-29 14:51:10 +0100

    upload photo dans détails

--- src/app/profil-details/profil-details.page.ts
+++ src/app/profil-details/profil-details.page.ts
@@ -5,6 +5,13 @@ import {FirebaseService} from '../services/firebase.service';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireStorage} from '@angular/fire/storage';
+
+export interface Image {
+    id: string;
+    image: string;
+}
 
 @Component({
     selector: 'app-profil-details',
@@ -14,7 +21,11 @@ import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms'
 export class ProfilDetailsPage implements OnInit {
 
     validationsForm: FormGroup;
-    url: String="/assets/images/post-malone.jpg";
+    urlImage: String="/assets/images/post-malone.jpg";
+    newImage: Image = {
+        id: this.afs.createId(), image: ''
+    }
+    loading: boolean = false;
 
     constructor(
         private menu: MenuController,
@@ -23,12 +34,17 @@ export class ProfilDetailsPage implements OnInit {
         private fireService: FirebaseService,
         private router: Router,
         private formBuilder: FormBuilder,
+        private afs: AngularFirestore,
+        private storage: AngularFireStorage
     ) {
     }
 
     ngOnInit() {
         this.initField();
         this.validationsForm = this.formBuilder.group({
+            url: new FormControl('', Validators.compose([
+                Validators.required
+            ])),
             nom: new FormControl('', Validators.compose([
                 Validators.required
             ])),
@@ -71,14 +87,55 @@ export class ProfilDetailsPage implements OnInit {
         }
     }
 
+    uploadImage(event) {
+        this.loading = true;
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+
+            reader.readAsDataURL(event.target.files[0]);
+            // For Preview Of Image
+            reader.onload = (e:any) => { // called once readAsDataURL is completed
+                this.urlImage = e.target.result;
+
+                // For Uploading Image To Firebase
+                const fileraw = event.target.files[0];
+                console.log(this.newImage.id);
+                const filePath = '/Image/'+ this.newImage.id + '/'+ this.newImage.id;
+                const result = this.SaveImageRef(filePath, fileraw);
+                const ref = result.ref;
+                result.task.then(a => {
+                    ref.getDownloadURL().subscribe(a => {
+                        console.log(a);
+                        this.validationsForm.get('url').setValue(a);
+                        this.newImage.image = a;
+                        this.loading = false;
+                    });
+
+                    this.afs.collection('Image').doc(this.newImage.id).set(this.newImage);
+                });
+            }, error => {
+                alert("Error");
+            }
+
+        }
+    }
+
+    SaveImageRef(filePath, file) {
+
+        return {
+            task: this.storage.upload(filePath, file)
+            , ref: this.storage.ref(filePath)
+        };
+    }
+
     initField() {
         let that = this;
         this.fireService.getUserInformation().then(function(value) {
             that.validationsForm.get('nom').setValue(value.nom);
             that.validationsForm.get('prenom').setValue(value.prenom);
             that.validationsForm.get('email').setValue(value.email);
-            that.url = value.url;
-            console.log(value.url);
+            that.urlImage = value.url;
+            that.validationsForm.get('url').setValue(that.urlImage);
         });
 
     }
