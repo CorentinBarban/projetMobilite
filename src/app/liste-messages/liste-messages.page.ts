8ec53e68d67dba0e7a8b9e5a734df7f1390aa225
Mathieu <math.stiv@gmail.com>
2019-12-01 19:00:07 +0100

    Fonction de consultation de message

--- src/app/liste-messages/liste-messages.page.ts
+++ src/app/liste-messages/liste-messages.page.ts
@@ -22,23 +22,26 @@ export class ListeMessagesPage implements OnInit {
     ) {
         this.activatedRoute.params.subscribe((res) => {
             this.idLieu = res['location'];
-            this.lat = this.idLieu.substr(0, this.idLieu.indexOf('&'));
-            this.lng = this.idLieu.substr(this.idLieu.indexOf('&') + 1, this.idLieu.length);
+            this.lat = +this.idLieu.substr(0, this.idLieu.indexOf('&'));
+            this.lng = +this.idLieu.substr(this.idLieu.indexOf('&') + 1, this.idLieu.length);
             console.log('Lat : ' + this.lat + ' Lng : ' + this.lng);
         });
     }
 
     ngOnInit() {
 
+        this.initField(this.items);
+
     }
 
-    initField(items, location) {
+    initField(items) {
         var that = this;
 
         this.firebaseService.getAllMarkers().then(function (lieux) { // Récupérer les lieux, voir si lat & lgt sont les mêmes , récupérer le message
             for (let keyM of Object.keys(lieux)) {
                 let lieu = lieux[keyM];
-                if (lieu.lat == that.lat && lieu.lgt == that.lng) {
+                console.log(Math.round(lieu.lat * 100) / 100 + ' ' + Math.round(that.lat * 100) / 100 + ' ' + Math.round(lieu.lgt * 100) / 100 + ' ' + Math.round(that.lng * 100) / 100); // Il faut arrondir la valeur
+                if (Math.round(lieu.lat * 100) / 100 == Math.round(that.lat * 100) / 100 && Math.round(lieu.lgt * 100) / 100 == Math.round(that.lng * 100) / 100) {
                     items.push({
                         message: lieu.message,
                         note: lieu.idUser,
